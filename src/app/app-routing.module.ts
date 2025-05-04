dba36b64358549279204f2f9d5338db7f0e25b28
Jaime <jaimechacons@hotmail.com>
2025-05-04 19:28:11 +0200

    Position adj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions  } from '@angular/router';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', loadChildren: () => import('./pages/home.module').then(module => module.HomeModule)}, // lazy load
 ];
 
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',  // Habilita la restauración de la posición del scroll
+  anchorScrolling: 'enabled',           // Habilita el desplazamiento a los elementos de anclaje (#)
+};
 
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
